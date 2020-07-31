a10ac3d0c290f6c737bb8f54dbb7d3964359531d
Dinh Phu <ndp17hp@gmail.com>
2020-07-31 15:08:52 +0700

    driver post gree

--- src/main/java/com/dinhpu/m4casestudy/M4casestudyApplication.java
+++ src/main/java/com/dinhpu/m4casestudy/M4casestudyApplication.java
@@ -3,12 +3,13 @@ package com.dinhpu.m4casestudy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class M4casestudyApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SpringApplication.run(M4casestudyApplication.class, args);
-	}
+        SpringApplication.run(M4casestudyApplication.class, args);
+    }
 
 }
