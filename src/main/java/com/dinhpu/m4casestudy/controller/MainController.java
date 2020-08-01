7ee80715bc6b1d6c1521a803d457cf726009a8a9
Dinh Phu <ndp17hp@gmail.com>
2020-08-01 22:43:41 +0700

    xong phan trang all post home page

--- src/main/java/com/dinhpu/m4casestudy/controller/MainController.java
+++ src/main/java/com/dinhpu/m4casestudy/controller/MainController.java
@@ -1,15 +1,18 @@
 package com.dinhpu.m4casestudy.controller;
 
 import com.dinhpu.m4casestudy.model.real_estate.RealEstate;
+import com.dinhpu.m4casestudy.model.user.User;
 import com.dinhpu.m4casestudy.services.real_estate.IRealEstateServices;
 import com.dinhpu.m4casestudy.services.real_estate.RealEstateServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,6 +22,8 @@ public class MainController {
 
 	@Autowired
 	private IRealEstateServices realEstateServices;
+
+	private int PAGE_SIZE=5;
 
 	@GetMapping("/")
 	public String showHome(Model theModel) {
@@ -34,7 +39,22 @@ public class MainController {
 	}
 	
 
-	
+	@GetMapping("/all-post/{pageNo}")
+	public String showAllPost(@PathVariable int pageNo,Model theModel){
+
+		Optional<String> sortBy= Optional.of("id");
+
+		Page<RealEstate> page=realEstateServices.findAll(pageNo,PAGE_SIZE,sortBy);
+		List<RealEstate> realEstates=page.getContent();
+
+		theModel.addAttribute("currentPage",pageNo);
+		theModel.addAttribute("totalPages",page.getTotalPages());
+		theModel.addAttribute("totalItems",page.getTotalElements());
+		theModel.addAttribute("listRealEstate",realEstates);
+
+		return "show-all-post";
+	}
+
 }
 
 
