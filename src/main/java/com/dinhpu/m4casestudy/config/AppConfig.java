3a1a0550f424cd4188685b8920fd8831a6afb236
Dinh Phu <ndp17hp@gmail.com>
2020-08-06 16:31:16 +0700

    bat dau cai dat de up file len s3

--- src/main/java/com/dinhpu/m4casestudy/config/AppConfig.java
+++ src/main/java/com/dinhpu/m4casestudy/config/AppConfig.java
@@ -1,6 +1,12 @@
 package com.dinhpu.m4casestudy.config;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -20,4 +26,26 @@ public class AppConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/upload_file/**").addResourceLocations("file:/"+uploadPath+"/");
 
     }
+
+    @Value("${jsa.aws.access_key_id}")
+    private String awsId;
+
+    @Value("${jsa.aws.secret_access_key}")
+    private String awsKey;
+
+    @Value("${jsa.s3.region}")
+    private String region;
+
+    @Bean
+    public AmazonS3 s3client() {
+
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials(awsId, awsKey);
+        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                .withRegion(Regions.fromName(region))
+                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+                .build();
+
+        return s3Client;
+    }
+
 }
